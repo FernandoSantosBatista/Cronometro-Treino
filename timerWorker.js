7e260c7928f48955eb1f8e7d329b772b54422ebe
Fernando dos Santos Batista <fernandodossantos005@gmail.com>
2024-10-21 09:54:41 -0300

    Update timerWorker.js

--- timerWorker.js
+++ timerWorker.js
@@ -3,26 +3,18 @@ let endTime;
 let pausedTime;
 let timeRemaining;
 
-let totalTimer;
-let totalStartTime;
-let totalPausedTime = 0;  // Para salvar o tempo quando o total for pausado
-
 onmessage = function (e) {
   const { command, selectedTime } = e.data;
 
   if (command === 'start') {
     if (countdownTimer) clearInterval(countdownTimer);
-    
+
+    // Se o tempo estava pausado, retoma a partir de onde parou
     if (pausedTime) {
       endTime = Date.now() + pausedTime * 1000;
     } else {
       timeRemaining = selectedTime;
       endTime = Date.now() + selectedTime * 1000;
-    }
-
-    // Inicia o cronômetro de tempo total se ainda não estiver rodando
-    if (!totalStartTime) {
-      totalStartTime = Date.now() - totalPausedTime;
     }
 
     countdownTimer = setInterval(() => {
@@ -35,31 +27,18 @@ onmessage = function (e) {
       } else {
         postMessage({ timeRemaining });
       }
-
-      // Calcula o tempo total
-      const totalTimeElapsed = Math.floor((now - totalStartTime) / 1000);
-      postMessage({ totalTimeElapsed });
-    }, 100);
+    }, 100);  // Atualize a cada 100ms para maior precisão
   }
 
   if (command === 'pause') {
     clearInterval(countdownTimer);
-    pausedTime = timeRemaining;
-    
-    if (totalStartTime) {
-      totalPausedTime = Date.now() - totalStartTime;  // Calcula quanto tempo total já passou antes de pausar
-    }
+    pausedTime = timeRemaining;  // Salva o tempo restante ao pausar
   }
 
   if (command === 'reset') {
     clearInterval(countdownTimer);
-    pausedTime = null;
+    pausedTime = null;  // Reseta qualquer tempo pausado
     timeRemaining = selectedTime;
     postMessage({ timeRemaining: selectedTime });
-    
-    // Reseta o cronômetro total
-    totalStartTime = null;
-    totalPausedTime = 0;
-    postMessage({ totalTimeElapsed: 0 });
   }
 };
