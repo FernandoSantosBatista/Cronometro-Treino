f2b91a62dfead1779a61390c57389d47e6f8fcea
Fernando dos Santos Batista <fernandodossantos005@gmail.com>
2024-10-21 08:40:30 -0300

    Update timerWorker.js

--- timerWorker.js
+++ timerWorker.js
@@ -1,4 +1,6 @@
 let countdownTimer;
+let endTime;
+let pausedTime;
 let timeRemaining;
 
 onmessage = function (e) {
@@ -7,25 +9,35 @@ onmessage = function (e) {
   if (command === 'start') {
     if (countdownTimer) clearInterval(countdownTimer);
 
-    timeRemaining = selectedTime;
-    
+    // Se o tempo estava pausado, retoma a partir de onde parou
+    if (pausedTime) {
+      endTime = Date.now() + pausedTime * 1000;
+    } else {
+      timeRemaining = selectedTime;
+      endTime = Date.now() + selectedTime * 1000;
+    }
+
     countdownTimer = setInterval(() => {
-      timeRemaining--;
-      postMessage({ timeRemaining });
+      const now = Date.now();
+      timeRemaining = Math.round((endTime - now) / 1000);
 
       if (timeRemaining <= 0) {
         clearInterval(countdownTimer);
         postMessage({ timeRemaining: 0 });
+      } else {
+        postMessage({ timeRemaining });
       }
-    }, 1000);
+    }, 100);  // Atualize a cada 100ms para maior precisão
   }
 
   if (command === 'pause') {
     clearInterval(countdownTimer);
+    pausedTime = timeRemaining;  // Salva o tempo restante ao pausar
   }
 
   if (command === 'reset') {
     clearInterval(countdownTimer);
+    pausedTime = null;  // Reseta qualquer tempo pausado
     timeRemaining = selectedTime;
     postMessage({ timeRemaining: selectedTime });
   }
